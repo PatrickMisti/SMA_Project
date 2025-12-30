67c2a341b3fc2508a56a15fc890f11217794c98f
PatrickMisti <patrickm99@hotmail.de>
2025-12-30 19:31:32 +0100

    add gitignore and update config wrapper

--- BrokeFlix/brokeflix_client/lib/core/shared/utils/config_wrapper.dart
+++ BrokeFlix/brokeflix_client/lib/core/shared/utils/config_wrapper.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class ConfigWrapper {
+class ConfigWrapper {     //  API_URL_vLocal  //  API_URL
   static final _apiUrl = dotenv.maybeGet('API_URL') ?? FileSystemException("API_Url not found!");
 
   static String get popularSeriesUrl => '$_apiUrl/api/serienstream/popular';
