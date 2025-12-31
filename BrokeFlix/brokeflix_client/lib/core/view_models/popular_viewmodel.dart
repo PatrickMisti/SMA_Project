f0c32204a4668b7973622a663d32a2b3585fc695
PatrickMisti <patrickm99@hotmail.de>
2025-12-31 12:16:34 +0100

    removed import

--- BrokeFlix/brokeflix_client/lib/core/view_models/popular_viewmodel.dart
+++ BrokeFlix/brokeflix_client/lib/core/view_models/popular_viewmodel.dart
@@ -2,7 +2,6 @@ import 'package:brokeflix_client/core/shared/data_service.dart';
 import 'package:brokeflix_client/core/shared/models/series_model.dart';
 import 'package:brokeflix_client/core/shared/utils/app_logger.dart';
 import 'package:brokeflix_client/core/shared/utils/interfaces.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:get_it/get_it.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:stacked/stacked.dart';
